78758df969995f949601ff0f77d0f07b89712d99
RakshathaAttuluri <rakshatha.attuluri@gmail.com>
2023-11-20 20:50:05 -0800

    Fix random paste called after every epoch

--- dataset.py
+++ dataset.py
@@ -8,34 +8,43 @@ from torchvision import transforms
 
 
 class TurtleDataset(Dataset):
-    def __init__(self, bg_img_dir: str, fg_img_path: str,
-                 transform=None) -> None:
+    def __init__(self, bg_img_dir: str, fg_img_path: str):
         super().__init__()
-        self.bg_img_paths = glob.glob(f'{bg_img_dir}/*.png')
-        self.fg_img = Image.open(fg_img_path).resize((256, 256))
-        self.transform = transform
-        if transform == None:
-            self.transform = transforms.Compose([
-                transforms.Pad(4),
-                transforms.ToTensor()
-            ])
+        self.images = self._get_images(bg_img_dir, fg_img_path)
+        self.transform = transforms.Compose([
+            transforms.Pad(4),
+            transforms.ToTensor()
+        ])
 
     def __len__(self):
-        return len(self.bg_img_paths)
+        return len(self.images)
     
     def __getitem__(self, index):
-        img = Image.open(self.bg_img_paths[index])
-        img, seg_mask = self._random_paste(img)
+        img, seg_mask = self.images[index]
+        img = self.transform(img)[:3]
+
+        seg_mask = transforms.ToTensor()(seg_mask)
+        seg_mask = (seg_mask[-1:] > 0).to(torch.float32)
+
         return (img, seg_mask)
 
-    def _random_paste(self, background_image, min_scale=0.25, max_scale=0.65):
+    def _get_images(self, bg_img_dir, fg_img_path):
+        bg_img_paths = glob.glob(f'{bg_img_dir}/*.png')
+        fg_img = Image.open(fg_img_path).resize((256, 256))
+        images = []
+        for img_path in bg_img_paths:
+            img = Image.open(img_path)
+            images.append(self._random_paste(img, fg_img))
+
+        return images
+
+    def _random_paste(self, background_image, fg_img, min_scale=0.25, max_scale=0.65):
         """Randomly scales and pastes the turtle image onto the background image"""
-    
-        w, h = self.fg_img.size
+        w, h = fg_img.size
         # first, we will randomly downscale the turtle image
         new_w = int(random.uniform(min_scale, max_scale) * w)
         new_h = int(random.uniform(min_scale, max_scale) * h)
-        resized_turtle_image = self.fg_img.resize((new_w, new_h))
+        resized_turtle_image = fg_img.resize((new_w, new_h))
 
         # second, will randomly choose the locations where to paste the new image
         start_w = random.randint(0, w - new_w)
@@ -53,15 +62,4 @@ class TurtleDataset(Dataset):
         # finally, will paste the resized turtle onto the background image
         background_image.paste(resized_turtle_image, (start_w, start_h), resized_turtle_image)
 
-        # Prepare background image.
-        background_image = self.transform(background_image)[:3]
-
-        # Prepare one hot encoded foreground image.
-        canvas_image = self._prep_canvas_image(canvas_image)
-        return background_image, canvas_image
-
-    def _prep_canvas_image(self, seg_mask):
-        seg_mask = transforms.ToTensor()(seg_mask)
-        seg_mask = (seg_mask[-1:] > 0).to(torch.float32)
-
-        return seg_mask
+        return (background_image, canvas_image)
